4ffd2ff28bcbdbbbd54e21180737717cbe0ba6b7
uchrissd <uchrissd@protonmail.com>
2020-08-06 19:23:18 -0700

    added axios api code, currently commented out until i fix authentication

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const { App } = require("@slack/bolt");
+const axios = require("axios");
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
+
+// var config = {
+//   method: "get",
+//   url: "https://api.twitter.com/1.1/1286810786594082816/show.json",
+//   headers: {
+//     Authorization:
+//       "Bearer AAAAAAAAAAAAAAAAAAAAAFApGgEAAAAAtfLgnx16ANS0ojGzLQt2N%2FHIGds%3DsZtqlTGMqMlTyNpyD8XxaEWfrute8YsKsUNshB6KWgAp9tbMzf",
+//     Cookie:
+//       'personalization_id="v1_9UB28lmsoNJhwvm0kFQ+sQ=="; guest_id=v1%3A159659739364068268',
+//   },
+// };
+
+// axios(config)
+//   .then(function (response) {
+//     console.log(JSON.stringify(response.data));
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
 
 // Listens to incoming messages that contain "hello"
 app.message("hello", async ({ message, say }) => {
